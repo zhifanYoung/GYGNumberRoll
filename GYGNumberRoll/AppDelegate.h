00dd4ef3390ca4dcd31d42f1dcb55b5b88afa7fd
zhifanYoung <zhifan_young@163.com>
2016-04-01 15:42:16 +0800

    Initial Commit

---
+++ GYGNumberRoll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GYGNumberRoll
+//
+//  Created by Young on 16/4/1.
+//  Copyright © 2016年 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GYGNumberRoll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GYGNumberRoll
+//
+//  Created by Young on 16/4/1.
+//  Copyright © 2016年 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
